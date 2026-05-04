c7877803c1eb7eb67c13c6092e59b685526c9280
Aadarsha Subedi <subediaadarsha0@gmail.com>
2026-05-04 13:35:59 +0545

    Use environment API URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 // src/lib/axios.ts
 import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/v1";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/v1";
 
 // Create axios instance
 const axiosInstance = axios.create({
